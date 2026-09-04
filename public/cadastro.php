517434a1a72abca75d81a21df0f7beff5213d0ff
RoederArt <arthur_roeder@estudante.sesisenai.org.br>
2026-09-04 12:15:44 -0300

    up - inicio do cadastro

---
+++ public/cadastro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+
+<header></header>
+    
+<main>
+    <div>
+        <form>
+            <h1>CADASTRO</h1>
+            <label for=""></label>
+            <input type="text" placeholder="Digite seu nome completo"> 
+
+        </form>
+    </div>
+</main>
+
+<footer></footer>
+
+</body>
+</html>
